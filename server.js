8f36aa8031f398b54c8ae0539ee85e65e1751809
Glitch (material-foxglove) <none>
2019-01-03 21:26:21 +0000

    🎯🌃 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+const multer = require('multer')
+const upload = multer({ storage: multer.memoryStorage() })
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  console.log(req.file);
+  res.json({name:req.file.originalname,type:req.file.mimetype,size:req.file.size});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
